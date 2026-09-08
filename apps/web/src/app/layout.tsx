e4b6a7e6df3b79f9d88c5aa4d34e3a62771a763f
Louis Hinchliffe <louishinchliffe11@gmail.com>
2026-09-08 15:29:18 +0100

    Load converter motion polish

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 import "./converter-feedback.css";
+import "./converter-motion.css";
 import "./feedback-polish.css";
 import "./site-enhancements.css";
 import "./dark-mode.css";
