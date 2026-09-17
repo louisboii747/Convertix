23c80d91709e3947ce14b747e857e3162db44ca4
Louis Hinchliffe <louishinchliffe11@gmail.com>
2026-09-17 11:27:06 +0100

    split swift project into modular design

---
+++ apps/apple/ConvertixWidget/ConvertixWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  ConvertixWidgetBundle.swift
+//  ConvertixWidget
+//
+//  Created by Louis Hinchliffe on 17/09/2026.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct ConvertixWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        ConvertixWidget()
+    }
+}
