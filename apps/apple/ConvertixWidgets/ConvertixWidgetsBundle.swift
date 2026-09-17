6728ac6ca8015bd4a4769946b11143c5dd89d854
Louis Hinchliffe <louishinchliffe11@gmail.com>
2026-09-17 09:44:43 +0100

    add activitykit and widget support for iphone and update dev icon

---
+++ apps/apple/ConvertixWidgets/ConvertixWidgetsBundle.swift
@@ -0,0 +1,16 @@
+//
+//  ConvertixWidgetsBundle.swift
+//  ConvertixWidgets
+//
+//  Created by Louis Hinchliffe on 17/09/2026.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct ConvertixWidgetsBundle: WidgetBundle {
+    var body: some Widget {
+        ConvertixWidgets()
+    }
+}
